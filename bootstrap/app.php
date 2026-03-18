15514f80534d5606826759fabfa63643327b85e1
13cweb <info@13cweb.com>
2026-03-19 00:15:16 +0900

    20260318d

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,4 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create()->usePublicPath('/home/web13c/best-web.net/public_html/solo');
+    })->create()->usePublicPath(env('LARAVEL_PUBLIC_PATH', public_path()));
